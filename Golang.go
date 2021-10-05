9f862974ef3c14ed947231e4012b1b733d55db10
mashendragunawan <mashendragunawan04@gmail.com>
2021-10-05 19:44:10 +0700

    pertama

---
+++ Golang.go
@@ -0,0 +1,31 @@
+package main
+
+func main() {
+
+	// var num1 int
+	// var num2 int
+
+	// fmt.Print("Masukan Angka Pertama : ")
+	// fmt.Scanf("%d", &num1)
+
+	// fmt.Print("Masukan Angka Kedua :")
+	// fmt.Scanf("%d", &num2)
+
+	// pl := num1 / num2
+
+	// fmt.Println(num1, ":", num2, "=", pl)
+
+	// var nim int
+	// var nama string
+
+	// fmt.Print("Masukan Nama Nim : ")
+	// fmt.Scanf("%d", &nim)
+
+	// fmt.Print("Maukan Nama Anda : ")
+	// fmt.Scanf("%s", &nama)
+
+	// fmt.Println("==========================")
+
+	// fmt.Println("Nim :", nim)
+	// fmt.Println("Nama :", nama)
+}
